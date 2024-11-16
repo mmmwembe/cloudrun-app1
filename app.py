3edef98f70271967f1adcff1c2b7be47f5f2fb01
Michael M <mmm111@hotmail.com>
2024-11-15 19:32:04 -0500

    changes

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, send_file, jsonify, session, redirect, url_for , flash, send_from_directory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+app.config['SECURITY_PASSWORD_SALT']='thisistheSALTforcreatingtheCONFIRMATIONtoken'
+
+bucket_name ='papers-bucket-mmm'
+session_id = 'eb9db0ca54e94dbc82cffdab497cde13'
+sample_id = '8c583173bc904ce596d5de69ac432acb'
+
 
 @app.route('/')
 def home():
-    long_string = os.getenv('LONG_STRING', 'Environment variable not set')
-    return f'<h1>LONG_STRING value:</h1><p>{long_string}</p>'
+    #long_string = os.getenv('LONG_STRING', 'Environment variable not set')
+    #return f'<h1>LONG_STRING value:</h1><p>{long_string}</p>'
+    return render_template('upload_images.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
---
+++ modules/gcp_ops.py
@@ -0,0 +1,65 @@
+
+import os
+import json
+from google.cloud import storage
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the Google service account JSON from environment variable
+secret_json = os.getenv('GOOGLE_SECRET_JSON')
+
+
+bucket_name ='flask-test-bucket-mmm'
+session_id = 'eb9db0ca54e94dbc82cffdab497cde13'
+sample_id = '8c583173bc904ce596d5de69ac432acb'
+
+
+def save_file_to_bucket(artifact_url, session_id, sample_id, bucket_name, subdir="papers", subsubdirs=["pdf","word","images","csv","text"]):
+    # Determine the subsubdir based on the file extension
+    if artifact_url.endswith(".docx"):
+        subsubdir = "word"
+    elif artifact_url.endswith((".jpg", ".jpeg", ".png", ".gif")):
+        subsubdir = "images"
+    elif artifact_url.endswith(".csv"):
+        subsubdir = "csv"
+    elif artifact_url.endswith((".txt", ".text")):
+        subsubdir = "text"
+    else:
+        subsubdir = "pdf"  # Default to PDF if no other match
+
+    client = storage.Client.from_service_account_info(json.loads(secret_json))
+
+    if subsubdir == "word":
+            # Delete the contents of the subsubdir before uploading the new file
+            blob_prefix = f"{session_id}/{sample_id}/{subdir}/{subsubdir}/"
+            bucket = client.bucket(bucket_name)
+            blobs = client.list_blobs(bucket, prefix=blob_prefix)
+            for blob in blobs:
+                blob.delete()
+
+    try:
+        # Upload the file to Google Cloud Storage
+        blob_name = f"{session_id}/{sample_id}/{subdir}/{subsubdir}/{os.path.basename(artifact_url)}"
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        blob.upload_from_filename(artifact_url)
+        #print(f"File uploaded to: gs://{bucket_name}/{blob_name}")
+        url = blob.public_url
+        #print(f"Public URL: {url}")
+        return url
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+
+
+def get_public_urls(bucket_name, session_id, sample_id):
+    
+    client = storage.Client.from_service_account_info(json.loads(secret_json))
+    
+    bucket = client.bucket(bucket_name)
+    
+    blobs = bucket.list_blobs(prefix=f"{session_id}/{sample_id}/")
+    
+    return [f"https://storage.googleapis.com/{bucket_name}/{blob.name}" for blob in blobs]
