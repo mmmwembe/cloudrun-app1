1f2e66f46f3ceaefbe35bf0ddd9a3e43a1d8b142
mmmwembe <mmm111@hotmail.com>
2024-11-24 08:38:43 -0500

    impressed

--- modules/llm_ops.py
+++ modules/llm_ops.py
@@ -26,10 +26,11 @@ secret_json = os.getenv('GOOGLE_SECRET_JSON')
 BUCKET_EXTRACTED_IMAGES = 'papers-extracted-images-bucket-mmm'
 SESSION_ID = 'eb9db0ca54e94dbc82cffdab497cde13'
 
-models_array = ["mixtral-8x7b-32768","llama3-70b-8192","llama3-8b-8192","gemma-7b-it","gemma2-9b-it"]
+#models_array = ["mixtral-8x7b-32768","llama3-70b-8192","llama3-8b-8192","gemma-7b-it","gemma2-9b-it"]
+models_array = models = ["gemma-7b-it", "gemma2-9b-it", "llama-3.1-70b-versatile", "llama-3.1-8b-instant", "llama-3.2-11b-text-preview", "llama-3.2-11b-vision-preview", "llama-3.2-1b-preview", "llama-3.2-3b-preview", "llama-3.2-90b-text-preview", "llama-3.2-90b-vision-preview", "llama-guard-3-8b", "llama3-70b-8192", "llama3-8b-8192", "llama3-groq-70b-8192-tool-use-preview", "llama3-groq-8b-8192-tool-use-preview", "llava-v1.5-7b-4096-preview", "mixtral-8x7b-32768"]
 
 llm = ChatGroq(
-    model="mixtral-8x7b-32768",
+    model="llava-v1.5-7b-4096-preview", #"mixtral-8x7b-32768",
     temperature=0,
     max_tokens=None,
     timeout=None,
